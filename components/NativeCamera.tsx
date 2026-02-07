1ba2c8dcb869bae873328edd8048d967eef107b2
Rovakarl <karlmartinot@gmail.com>
2026-02-07 15:04:06 +0300

    alert sv

--- components/NativeCamera.tsx
+++ components/NativeCamera.tsx
@@ -39,7 +39,7 @@ export default function NativeCamera({
       if (!result.granted) {
         Alert.alert(
           "Permission requise",
-          "L'application a besoin d'accéder à votre caméra svp."
+          "L'application a besoin d'accéder à votre caméra sv."
         );
         return;
       }
